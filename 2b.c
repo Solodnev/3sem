f28b59bfb9701252e947a76673411ffb7131406a
Stepan <Solodnev Stepan>
2016-09-29 21:20:12 +0300

    first commit

---
+++ 2b.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char **argv)
+{	
+	int i;
+	int N = 5;
+	for (i = 0; i < N; ++i){
+		pid_t pid = fork ();
+		if (pid == 0) {
+			printf("I am a child and my PID is: %d\n", getpid());
+			sleep(i+1);
+			printf("%d\n", i);
+		} if (pid != 0) {
+			printf("I am a parent of %d, my PID is %d and I am waiting for a child\n", pid, getpid());
+			int status = 0;
+			waitpid(pid, &status, 0);
+			printf("I am a parent of %d, my PID is %d and my child has finished\n", pid, getpid());
+			exit(0);
+		}
+	}
+	return 0;
+}
+
---
+++ 5.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int read_file(const char* filename, char** buf_addr, int* len);
+
+int main(int argc, char **argv)
+{
+	char* file_content;
+	int file_len = 0;
+	read_file("commands.txt", &file_content, &file_len);
+	
+	int is_odd = 1;
+	int timing = 0;
+
+	char* pch = strtok(file_content, "\n");
+    while (pch != NULL)
+    {
+		if (is_odd)
+		{
+			timing = atoi(pch);
+			is_odd = 0;
+		} else
+		{
+			pid_t pid = fork();
+			if (pid == 0)
+			{
+				char* command = strdup(pch);
+				char* pch2 = command;
+				int spaces = 0;
+				while (*pch2 != '\0')
+				{
+					if (*pch2 == ' ')
+					    ++spaces;
+					++pch2;
+				}
+				char** args = (char**) calloc(spaces + 2, sizeof(*args));
+				pch2 = command;
+				char* start_command = pch2;
+				int index = 0;
+				while (*pch2 != '\0')
+				{
+					if (*pch2 == ' ')
+					{
+						args[index] = start_command;
+						++index;
+						*pch2 = '\0';
+						start_command = pch2 + 1;
+					}
+					++pch2;
+				}
+				args[index] = start_command;
+				++index;
+				args[index] = NULL;
+				printf("Wait '%d'\n", timing);
+				sleep(timing);
+				printf("I am running program '%s'\n", args[0]);
+				execvp(args[0], args);
+				exit(0);
+			}
+			is_odd = 1;
+		}
+        pch = strtok(NULL, "\n");
+	}
+
+	free(file_content);
+	return 0;
+}
+
+int read_file(const char* filename, char** buf_addr, int* len)
+{
+    FILE* stream = fopen(filename, "rb");
+    if (!stream)
+        return 1;
+    if (fseek(stream, 0, SEEK_END))
+        return 2;
+
+    *len = ftell(stream);
+    rewind(stream);
+    char* buffer = (char*) calloc(*len, sizeof(*buffer));
+    fread(buffer, *len, sizeof(*buffer), stream);
+    fclose(stream);
+    *buf_addr = buffer;
+    return 0;
+}
